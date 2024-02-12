0692fd45335cb5121cc52535bd2499df7e83a88b
sathish19961412 <sathishfreelanc5@gmail.com>
2024-02-12 02:27:23 -0800

    parent and child Componets to the functional components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
+import { Fragment } from 'react';
 import './App.css';
 
 function App() {
   return (
+    <Fragment>
     <h1>Hello,'sathish'</h1>
+    <h1>Hello,'mathew'</h1>
+    </Fragment>
   );
 }
 
