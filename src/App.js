b449f3cbc384c14160107f453bc5d198d44dcccc
sathish19961412 <sathishfreelanc5@gmail.com>
2024-02-19 01:52:08 -0800

    start to the reactjs class components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Fragment } from 'react';
 import './App.css';
 import Cars from './components/Cars';
 import Garage from './components/Garage';
+import Apple from './components/Apple';
 function App() {
   return (
     <Fragment>
       <Cars />
       <Garage />
+      <Apple />
     </Fragment>
   );
 }
---
+++ src/components/Apple.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Apple extends React.Component
+{
+  render(){
+    return(
+       <h3>Hi,I am Apple</h3>
+    );
+      
+  }
+  
+}
+
+export default Apple;
