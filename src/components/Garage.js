e2586ccc87f1ba217079608446df3207ce43c5dd
sathish19961412 <sathishfreelanc5@gmail.com>
2024-02-27 04:42:19 -0800

    completed to the conditional rendering the components

--- src/components/Garage.js
+++ src/components/Garage.js
@@ -2,22 +2,27 @@ import React, { Fragment } from "react";
 import Apple from './Apple';
 import Cars from "./Cars";
 function Garage(){
+    const isDoorOpened=false;
     // const brand='Ford';
     // const color="Black";
     const Carinfo={
         brand:"Ford",
         color:"red"
     }
+    // const Carinfo={};
     const appleInfo={
         type:"Fuji",
         color:"green"
     }
-    return(
+    const showCarInfo = Carinfo.brand !==undefined && Carinfo.color !==undefined;
+    return(       
         <Fragment>
             <h2>Who lives inside my Garage?</h2>
             {/* <Cars brand={brand} color={'Black'} /> */}
-            <Cars Carinfo={Carinfo}/>
+            {/* { showCarInfo?<Cars Carinfo={Carinfo}/>:null} */}
+            { showCarInfo && <Cars Carinfo={Carinfo}/>}
             <Apple appleInfo={appleInfo} />
+            {isDoorOpened?<h2>Garage door is Open</h2>:<h2>Garage door is Close</h2>}
         </Fragment>
     );
 }
